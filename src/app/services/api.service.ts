22b4f63f20b0ed93cc51874d46f9d57bd8166bc6
Arambhadiya Sumit M <socet126@gmail.com>
2026-05-17 22:33:15 +0530

    chore: change backend API URL

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ApiResponse } from '../models/api-response.interface';
+// import { API_BASE_URL } from '../app.constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'https://localhost:8443/api';
+  private baseUrl = 'http://localhost:8080/api';
 
   constructor(private http: HttpClient) {}
 
